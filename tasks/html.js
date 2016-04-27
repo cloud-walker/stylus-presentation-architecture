985937e1c1ba9657a6d6f61d17f42feb9e6c7395
cloud-walker <baro.luc@gmail.com>
2016-04-27 21:36:55 +0200

    switch to pug template engine

--- tasks/html.js
+++ tasks/html.js
@@ -1,10 +1,12 @@
 import gulp from 'gulp'
+import pug from 'gulp-pug'
 import sync from 'browser-sync'
 import watch from 'gulp-watch'
 import {src, dest} from '../places'
 
 gulp.task('html', () =>
-	gulp.src(`${src}/index.html`)
+	gulp.src(`${src}/index.pug`)
+		.pipe(pug())
 		.pipe(gulp.dest(dest))
 		.pipe(sync.stream())
 )
